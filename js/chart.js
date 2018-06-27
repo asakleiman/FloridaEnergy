f4d0771c5eda156172eadde130f546871e2cca1b
kleinlowe <studio.retrograde@gmail.com>
2018-06-27 04:17:58 -0700

    Added documents forgotten previously

--- js/chart.js
+++ js/chart.js
@@ -4,17 +4,46 @@
 google.charts.load('current', {'packages':['corechart', 'timeline']})
 
 // create request object for data
-let request = new XMLHttpRequest()
-// open a new connection
+function getUseData(){
+  // Create a new request object
+  let request = new XMLHttpRequest()
+  // TODO: URL to contact goes here
+  let requestUrl = "https://api.eia.gov/series/?api_key=19f161a829f50e795db12f9a3c37b270&series_id=SEDS.TETCB.FL.A"
+  // Open a connection
+  request.open('GET', requestUrl, true)
+  // Callback for when the request completes
+  request.onload = function(){
+    let theActualData = JSON.parse(request.response).series[0].data
+    drawChartLine(theActualData)
+  }
+  // Callback for when there's an error
+  request.error = function(err){
+    console.log("error is: ", err)
+  }
+  // Send the request to the specified URL
+  request.send()
+}
 
-// callback for when request completes
+function getRenewData(){
+  // Create a new request object
+  let request = new XMLHttpRequest()
+  // TODO: URL to contact goes here
+  let requestUrl = "https://api.eia.gov/series/?api_key=19f161a829f50e795db12f9a3c37b270&series_id=SEDS.TETCB.FL.A"
+  // Open a connection
+  request.open('GET', requestUrl, true)
+  // Callback for when the request completes
+  request.onload = function(){
+    let theActualData = JSON.parse(request.response).series[0].data
+    drawChartLine(theActualData)
+  }
+  // Callback for when there's an error
+  request.error = function(err){
+    console.log("error is: ", err)
+  }
+  // Send the request to the specified URL
+  request.send()
+}
 
-// send the request
-
-// combine data objects into one object to display two lines on one chart
-
-
-// draw the chart
 
 google.charts.setOnLoadCallback(drawChart1);
 
